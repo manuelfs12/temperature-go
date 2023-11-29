078183be863ff10dabfc3b85a08387599b078d82
Manuel Figueroa <manuelfigueroa402@gmail.com>
2023-11-29 18:50:39 -0400

    add validation of user input

--- main.go
+++ main.go
@@ -3,23 +3,35 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	var userInput string
 	fmt.Println("Temperature GO, a CLI tool to convert temperatures.")
+	fmt.Println("#------------------------------------------------#")
+	fmt.Println("Accepted inputs: <number><c | f | k>, Eg: 50c ")
 	fmt.Printf("Please enter a temperature: ")
 	_, err := fmt.Scan(&userInput)
 	if err != nil {
 		return
 	}
-	convertedInput, _ := strconv.ParseFloat(userInput, 32)
-	fmt.Printf("%v° Celsius is %v° Kelvin\n", userInput, convertCelsiusToKelvin(convertedInput))
-	fmt.Printf("%v° Celsius is %v° Fahrenheit\n", userInput, convertCelsiusToFahrenheit(convertedInput))
-	fmt.Printf("%v° Kelvin is %v° Celsius\n", userInput, convertKelvinToCelsius(convertedInput))
-	fmt.Printf("%v° Kelvin is %v° Fahrenheit\n", userInput, convertKelvinToFahrenheit(convertedInput))
-	fmt.Printf("%v° Fahrenheit is %v° Celsius\n", userInput, convertFahrenheitToCelsius(convertedInput))
-	fmt.Printf("%v° Fahrenheit is %v° Kelvin\n", userInput, convertFahrenheitToKelvin(convertedInput))
+	userInput = strings.ToLower(userInput)
+	if strings.Contains(userInput, "c") {
+		userInput = strings.Trim(userInput, "c")
+		convertedInput, _ := strconv.ParseFloat(userInput, 32)
+		convertCelsius(userInput, convertedInput)
+	} else if strings.Contains(userInput, "f") {
+		userInput = strings.Trim(userInput, "f")
+		convertedInput, _ := strconv.ParseFloat(userInput, 32)
+		convertFahrenheit(userInput, convertedInput)
+	} else if strings.Contains(userInput, "k") {
+		userInput = strings.Trim(userInput, "k")
+		convertedInput, _ := strconv.ParseFloat(userInput, 32)
+		convertKelvin(userInput, convertedInput)
+	} else {
+		println("Invalid input")
+	}
 }
 
 func convertCelsiusToKelvin(temp float64) string {
@@ -44,4 +56,19 @@ func convertFahrenheitToCelsius(temp float64) string {
 
 func convertFahrenheitToKelvin(temp float64) string {
 	return fmt.Sprintf("%.2f", (temp-32)*5/9+273.15)
+}
+
+func convertCelsius(userInput string, convertedInput float64) {
+	fmt.Printf("%v° Celsius is %v° Kelvin\n", userInput, convertCelsiusToKelvin(convertedInput))
+	fmt.Printf("%v° Celsius is %v° Fahrenheit\n", userInput, convertCelsiusToFahrenheit(convertedInput))
+}
+
+func convertKelvin(userInput string, convertedInput float64) {
+	fmt.Printf("%v° Kelvin is %v° Celsius\n", userInput, convertKelvinToCelsius(convertedInput))
+	fmt.Printf("%v° Kelvin is %v° Fahrenheit\n", userInput, convertKelvinToFahrenheit(convertedInput))
+}
+
+func convertFahrenheit(userInput string, convertedInput float64) {
+	fmt.Printf("%v° Fahrenheit is %v° Celsius\n", userInput, convertFahrenheitToCelsius(convertedInput))
+	fmt.Printf("%v° Fahrenheit is %v° Kelvin\n", userInput, convertFahrenheitToKelvin(convertedInput))
 }
