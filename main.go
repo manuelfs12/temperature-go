1110391c4e25de84f6750b891034bcd08eba865a
Manuel Figueroa <manuelfigueroa402@gmail.com>
2023-11-29 17:31:42 -0400

    add formula to convert kelvin to celsius and kelvin to f

--- main.go
+++ main.go
@@ -8,11 +8,15 @@ import (
 func main() {
 	var userInput string
 	fmt.Println("Temperature GO, a CLI tool to convert temperatures.")
-	fmt.Println("Please enter a temperature:")
-	fmt.Scan(&userInput)
+	fmt.Printf("Please enter a temperature: ")
+	_, err := fmt.Scan(&userInput)
+	if err != nil {
+		return
+	}
 	convertedInput, _ := strconv.ParseFloat(userInput, 32)
 	fmt.Printf("%v° celsius is %v° kelvin\n", userInput, convertCelsiusToKelvin(convertedInput))
 	fmt.Printf("%v° celsius is %v° fahrenheit\n", userInput, convertCelsiusToFahrenheit(convertedInput))
+	fmt.Printf("%v Kelvin us %v Celsisus", userInput, convertKelvinToFahrenheit(convertedInput))
 }
 
 func convertCelsiusToKelvin(temp float64) float64 {
@@ -20,5 +24,13 @@ func convertCelsiusToKelvin(temp float64) float64 {
 }
 
 func convertCelsiusToFahrenheit(temp float64) float64 {
-	return temp*(9/5) + 32
+	return (temp * 9 / 5) + 32
+}
+
+func convertKelvinToCelsius(temp float64) float64 {
+	return temp - 273.15
+}
+
+func convertKelvinToFahrenheit(temp float64) float64 {
+	return (temp-273.15)*9/5 + 32
 }
